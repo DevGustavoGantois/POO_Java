60373f071a5c6eede778b6a8cd69772899b3a837
Gustavo Gantois <gugagantois@gmail.com>
2025-01-12 13:09:14 -0300

    feat:Creating new classe EssentialClasses5.

---
+++ essential_classes/EssentialClasses5.java
@@ -0,0 +1,12 @@
+package src.essential_classes;
+
+public class EssentialClasses5 {
+    public static void main(String[] args) {
+        String name = "Gustavo";
+        String name2 = "Gantois";
+        name.toLowerCase();
+        String valueName = name.toLowerCase(); //Mét0do que deixa todos os caracteres em minúsculo.
+        System.out.println(valueName);
+        String valueName2 = name2.split(); //é o metodo que quebra uma string por um delimitador assim como scanner, que me retornatra um array.
+    }
+}
