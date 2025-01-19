02adf6e4097453d75d946e77335125d26278ae42
Gustavo Gantois <gugagantois@gmail.com>
2025-01-19 17:55:01 -0300

    feat:Creating variable myAniversay.

--- java_time/LocalDateSimple.java
+++ java_time/LocalDateSimple.java
@@ -6,7 +6,10 @@ public class LocalDateSimple {
     public static void main(String[] args) {
         //Podemos discutir aspectos de fusos horários quando estamos falando de LocalDate Time.
         LocalDate actuallyDate = LocalDate.now();
+        //Data de aniversário
+        LocalDate myAniversary = LocalDate.of(2025,6,30);
 
         System.out.println(actuallyDate);
+        System.out.println(myAniversary);
     }
 }
