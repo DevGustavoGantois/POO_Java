1302bbb6026a19637ddc3618665291cb5ef2e872
Gustavo Gantois <gugagantois@gmail.com>
2025-01-19 19:15:38 -0300

    docs: commit the variable.

--- java_time/FormattedLocalDate.java
+++ java_time/FormattedLocalDate.java
@@ -7,7 +7,7 @@ public class FormattedLocalDate {
     public static void main(String[] args) {
         LocalDate date = LocalDate.of(2023, 1, 13);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy"); //Podemos usar vários tipos de formatações
 
         System.out.println(date); //2023-01-13
 
