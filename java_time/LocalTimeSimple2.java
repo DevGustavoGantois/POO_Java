ada20b3727459fbec33c4b97596af698b14ff978
Gustavo Gantois <gugagantois@gmail.com>
2025-01-19 19:48:57 -0300

    docs: Dados especificos.

--- java_time/LocalTimeSimple2.java
+++ java_time/LocalTimeSimple2.java
@@ -20,5 +20,12 @@ public class LocalTimeSimple2 {
         String formattedHour = hour.format(formatter);
         System.out.println(hour); //13:21:42
         System.out.println(formattedHour); //42-12-13
+
+        //Dados específicos
+        LocalTime hour2 = LocalTime.of(13, 21, 42);
+        System.out.println(hour2.getHour());
+        System.out.println(hour2.getMinute());
+        System.out.println(hour2.getSecond());
+        System.out.println(hour.getNano());
     }
 }
