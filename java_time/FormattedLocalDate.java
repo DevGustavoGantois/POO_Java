23a17fc75c8dd9ae322297d09bd037d545566a42
Gustavo Gantois <gugagantois@gmail.com>
2025-01-19 19:14:57 -0300

    feat:Creating new class FormattedLocalDate.

---
+++ java_time/FormattedLocalDate.java
@@ -0,0 +1,17 @@
+package src.java_time;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class FormattedLocalDate {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2023, 1, 13);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println(date); //2023-01-13
+
+        //Formatando a data criada acima
+        System.out.println(formatter.format(data));
+    }
+}
