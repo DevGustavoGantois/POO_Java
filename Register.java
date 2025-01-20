a852043037b371edc5bbead9a51492caa52bb08c
Gustavo Gantois <gugagantois@gmail.com>
2025-01-20 14:22:06 -0300

    feat:Create getters in class Register.

--- Register.java
+++ Register.java
@@ -1,5 +1,22 @@
 package src;
 
+import java.time.LocalDate;
+
 public class Register {
+    String name;
+    String sex;
+    Long phone;
+    LocalDate bornDate;
+    Double sugestValue;
+    boolean client;
+
+    public Register(String name, String sex, Long phone, LocalDate bornDate, Double sugestValue, boolean client) {
+        this.name = name;
+        this.sex = sex;
+        this.phone = phone;
+        this.bornDate = bornDate;
+        this.sugestValue = sugestValue;
+        this.client = client;
+    }
 
 }
