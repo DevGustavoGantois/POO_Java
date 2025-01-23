f5aac1792750bb973f878f08b1978bcc7afc8eec
Gustavo Gantois <gugagantois@gmail.com>
2025-01-23 10:28:57 -0300

    feat:Create new variable funcData in class expressions.

--- expressions/SimpleExpressions.java
+++ expressions/SimpleExpressions.java
@@ -4,8 +4,16 @@ public class SimpleExpressions {
     public static void main(String[] args) {
         String name = "Gustavo";
         String surname = "Gantois";
+        Integer register = 123;
+        Double salary = 2245.83;
         String text = "Hi ".concat(name).concat("").concat(surname);
 
+        String text_two = String.format("Hi %s %s", name, surname);
+        // s, d, f, t
         System.out.println(text);
+        System.out.println(text_two);
+
+        String funcData = String.format("Register: ¨%d - Name: %s - Salary: %,.2f", name, register, salary);
+        System.out.println(funcData);
     }
 }
