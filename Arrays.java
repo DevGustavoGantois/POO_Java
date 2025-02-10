8c90701345eeb4cbbd4806f114e39c48f8d9e438
Gustavo Gantois <gugagantois@gmail.com>
2025-02-10 11:58:57 -0300

    feat:Creating new class Array.

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Integer [] array = new Integer[6];
+        array[0] = 2;
+        array[1] = 4;
+        array[2] = 10;
+        array[3] = 15;
+        array[4] = 5;
+        array[5] = 3;
+        System.out.println("el.:" + array[3]);
+        for (int i = 0; i < array.length; i++) {
+            Integer element = array[i];
+            System.out.println(element);
+        }
+    }
+}
--- expressions/AdvancedExpressions.java
+++ expressions/AdvancedExpressions.java
@@ -1,12 +1,24 @@
 package src.expressions;
 
+import java.time.LocalDate;
+
 public class AdvancedExpressions {
     public static void main(String[] args) {
         String name = "Gustavo Gantois Caria Carvalho";
+        String surname = "Guga";
+        //Com a combinação de índices + position temos a capacidade de inverter as posições dos nossos argumentos:
+        System.out.println(String.format("%2$s %1$s", name, surname));
+
+
         Integer age = 21;
         Double salary = 3000.00;
-
-        String funcData = String.format("Age: %05d Name: %-15s Sal.: %,.2f", age, salary, name);
+        LocalDate AdmissionDate = LocalDate.now();
+        System.out.println(AdmissionDate);
+        //Através dos números após as porcentagens podemos definir quantos caracteres queremos q apareçam."
+        String funcData = String.format("Age: %05d Name: %-15.15s Sal.: %,.2f", age, salary, name);
         System.out.println(funcData);
+
+        //Formatando a data:
+        String funDateData = String.format("Date: %td/<%tm/<%tY", AdmissionDate, AdmissionDate);
     }
 }
